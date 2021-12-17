20b0d77ce0efd7d6e1bc0463e26a9f452bb63a3c
katomikiya <kato-m@omoteya.co.jp>
2021-12-17 13:52:36 +0900

    コンストラクタ

---
+++ day1/Sample101/Sample101/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample101
+{
+    internal class Person
+    {
+        private string name = "";
+        private int age = 0;
+
+        // コンストラクタ
+        public Person() : this("名前なし", 0)
+        {
+            Console.WriteLine("引数無しコンストラクタ");
+        }
+        // コンストラクタのオーバーロード
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            Console.WriteLine("引数ありコンストラクタ name: {0}, age: {1}", name, age);
+        }
+
+        public void showAgeAndName()
+        {
+            Console.WriteLine("名前：{0} 年齢：{1}", name, age);
+        }
+        // プロパティ
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public int Age
+        {
+            set { age = value; }
+            get { return age; }
+        }
+    }
+}
--- day1/Sample101/Sample101/Program.cs
+++ day1/Sample101/Sample101/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Person person2 = new Person("太田隆", 15);
+
+            person1.Name = "斎藤花子";
+            person1.Age = 18;
+
+            Console.WriteLine("名前：{0} 年齢：{1}", person1.Name, person1.Age);
+            person2.showAgeAndName();
+        }
+    }
+}
