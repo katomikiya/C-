1bdc0d4d05f31d5607d75e47e549741cbb08b4d3
katomikiya <kato-m@omoteya.co.jp>
2021-12-23 15:33:12 +0900

    Objectクラスと主要メソッド

---
+++ day3/Sample304/Sample304/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample304
+{
+    class Program
+    {
+        public void Foo()
+        {
+            Console.WriteLine("foo");
+        }
+
+        static void Main()
+        {
+            Program p = new Program();
+
+            Console.WriteLine(p.ToString());    // ToString()メソッド・・・オブジェクトを表す文字列を返す
+        }
+    }
+}
