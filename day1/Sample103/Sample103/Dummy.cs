95e1163da8905771a3eddf4bd73d06a8218b5b16
katomikiya <kato-m@omoteya.co.jp>
2021-12-23 11:31:13 +0900

    デストラクタ

---
+++ day1/Sample103/Sample103/Dummy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample103
+{
+    internal class Dummy
+    {
+        public Dummy()
+        {
+            Console.WriteLine("コンストラクタ");
+        }
+        ~Dummy()
+        {
+            Console.WriteLine("デストラクタ");
+        }
+    }
+}
---
+++ day1/Sample103/Sample103/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample103
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Dummy dummy = new Dummy();
+        }
+    }
+}
