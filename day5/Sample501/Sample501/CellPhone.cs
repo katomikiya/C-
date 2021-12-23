2e4e01c142ae9f006d41a7e8f1bba67210611c5e
katomikiya <kato-m@omoteya.co.jp>
2021-12-23 17:27:53 +0900

    インターフェースの実装

---
+++ day5/Sample501/Sample501/CellPhone.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample501
+{
+    internal class CellPhone    :Phone, Email
+    {
+        private string mail_address;
+        private string phone_number;
+        // コンストラクタ
+        public CellPhone(string adress, string number)
+        {
+            mail_address = adress;
+            phone_number = number;
+        }
+
+        public void SendEmail(string address)
+        {
+            Console.WriteLine(address + "に" + mail_address + "からメールを送信します。");
+        }
+        public void call(string number)
+        {
+            Console.WriteLine(number + "に" + phone_number + "から電話を掛けます。");
+        }
+
+    }
+}
---
+++ day5/Sample501/Sample501/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample501
+{
+    public class Program
+    {
+        static void Main()
+        {
+            CellPhone cp = new CellPhone("hoge@email.com", "090-1234-5678");
+
+            cp.call("080-987-6543");
+            cp.SendEmail("fuga@email.com");
+
+            // Phoneインターフェースでインスタンスにアクセス
+            Phone phone = (Phone)cp;
+            phone.call("052-111-2222");
+
+            // Emailインターフェースでインスタンスにアクセス
+            Email mail = (Email)cp;
+            mail.SendEmail("bar@email.com");
+        }
+    }
+}
