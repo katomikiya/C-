a9301413e3794debc213a462d550bf42c4191db8
katomikiya <kato-m@omoteya.co.jp>
2021-12-23 11:15:51 +0900

    ガベージコレクション

---
+++ day1/Sample102/Sample102/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample102
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[10000];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new string('M', 1000);
+            }
+
+            Console.WriteLine("メモリ使用量（GC発動前）:" + GC.GetTotalMemory(false));
+            // arrayの参照を解放
+            array = null;
+            Console.WriteLine("メモリ使用量（参照解除後）：" + GC.GetTotalMemory(false));
+            // ガレージコレクション
+            GC.Collect();
+            Console.WriteLine("メモリ使用量（GC発動後）：" + GC.GetTotalMemory(false));
+        }
+    }
+}
