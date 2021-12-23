5ce7a53b576ad422c869284804ed023ebb48285f
katomikiya <kato-m@omoteya.co.jp>
2021-12-23 14:50:08 +0900

    クラスの継承

---
+++ day3/Sample301/Sample301/ExCalc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample301
+{
+    // 親クラスを継承
+    internal class ExCalc : Calc  
+    {
+        public void mul()
+        {
+            Console.WriteLine("{0} * {1} = {2}", num1, num2, num1 * num2);
+        }
+        public void div()
+        {
+            Console.WriteLine("{0} / [1] = {2}", num1, num2, num1 / num2 );
+        }
+    }
+}
---
+++ day3/Sample301/Sample301/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample301
+{
+    public class Program
+    {
+        static void Main()
+        {
+        /* 親クラスCalcのインスタンス生成 */
+            Calc calc = new Calc();
+            // セッターで値を代入
+            calc.Num1 = 10;
+            calc.Num2 = 3;
+
+            calc.add();
+            calc.sub();
+
+        /* 子クラスExCalcのインスタンス生成 */
+            ExCalc ex_calc = new ExCalc();
+            ex_calc.Num1 = 20;
+            ex_calc.Num2 = 5;
+
+            ex_calc.add();
+            ex_calc.sub();
+            ex_calc.mul();
+            ex_calc.div();
+        }
+    }
+}
