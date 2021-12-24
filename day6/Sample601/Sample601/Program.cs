49a5f82acd65bda7306f2d56beefd9b62a2a40c2
katomikiya <kato-m@omoteya.co.jp>
2021-12-24 09:05:45 +0900

    CollectionクラスのListクラスで配列処理

---
+++ day6/Sample601/Sample601/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;  
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample601
+{
+    public class Program
+    {
+        static void Main()
+        {
+            List<int> list = new List<int>();
+
+            list.Add(3);
+            list.Add(2);
+            list.Add(1);
+
+            list.Insert(1, 100);    // 配列1番目に要素を追加
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine("list[{0}] = {1}", i, list[i]);
+            }
+        }
+    }
+}
