b0c7d486cf67f3e2bde394a50f2863901ab9471d
katomikiya <kato-m@omoteya.co.jp>
2021-12-27 11:51:53 +0900

    デリゲートの利用

---
+++ day7/Sample701/Sample701/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample701
+{
+    // デリゲートの宣言
+    delegate void Operation(int a, int b);
+
+    class Calc
+    {
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+        }
+    }
+
+    class Program
+    {
+        static void Add(int a, int b)
+        {
+            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+        }
+
+        static void Main()
+        {
+            Calc calc = new Calc();
+
+            // デリゲート型にメソッドを設定
+            Operation obj1 = new Operation(Add);
+            Operation obj2 = new Operation(calc.Sub);
+
+            obj1(5, 2);
+            obj2(10, 7);
+
+
+        }
+    }
+
+}
