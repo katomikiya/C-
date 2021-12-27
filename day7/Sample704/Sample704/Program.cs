a7c8eb74bf63d688d7e472a2765b98bef06361c5
katomikiya <kato-m@omoteya.co.jp>
2021-12-27 14:39:06 +0900

    throwによる例外発生

---
+++ day7/Sample704/Sample704/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample704
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                int result = getNum(4);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("配列の範囲外にアクセスしました。");
+            }
+        }
+
+        static int getNum(int i)
+        {
+            int[] nums = { 30, 600, 900 };
+
+            if (i < nums.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return nums[i];
+        }
+    }
+}
